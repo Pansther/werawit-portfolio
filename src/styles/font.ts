6694b3ed0e53796703e8b9e4ba6d10ff6b0ae3d7
Werawit Sirakunwat <werawit@edvisory.co.th>
2022-12-30 17:53:31 +0700

    chore: fix font display to swap

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -2,10 +2,14 @@ import { Noto_Sans, Noto_Sans_Thai } from '@next/font/google'
 
 export const notoSans = Noto_Sans({
   weight: '400',
+  display: 'swap',
+  subsets: ['latin'],
   variable: '--noto-sans-font',
 })
 
 export const notoSansTH = Noto_Sans_Thai({
   weight: '400',
+  display: 'swap',
+  subsets: ['latin'],
   variable: '--noto-sans-th-font',
 })
